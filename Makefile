1131f7b5cb69695f3482119cb1fd429c006600e1
bluesentinelsec <bluesentinel@protonmail.com>
2025-08-18 17:41:25 -0400

    Add test target to Makefile and enhance SDL initialization in engine.c
    - Added `test` target to Makefile, depending on `dev` target, to run test_leo_runtime
    - Modified engine.c to initialize SDL with additional subsystems (audio, joystick, gamepad, events)
    - Declared global SDL_Window and SDL_Renderer pointers

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
 dev:
 	cmake -B build -S . -DCMAKE_BUILD_TYPE=Debug -DLEO_BUILD_SHARED=ON -DLEO_BUILD_TESTS=On
 	cmake --build build --parallel
+
+test: dev
+	./build/test_leo_runtime
--- src/engine.c
+++ src/engine.c
@@ -3,10 +3,14 @@
 
 #include <SDL3/SDL.h>
 
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+
 
 bool leo_InitWindow(int width, int height, const char* title)
 {
-	if (!SDL_Init(SDL_INIT_VIDEO))
+	SDL_InitFlags flags = SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_JOYSTICK | SDL_INIT_GAMEPAD | SDL_INIT_EVENTS;
+	if (!SDL_Init(flags))
 	{
 		leo_SetError("%s\n", SDL_GetError());
 		return false;
