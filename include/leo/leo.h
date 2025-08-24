45e6fef8910f4af543688848e08f2b5790575b3e
bluesentinelsec <bluesentinel@protonmail.com>
2025-08-24 15:16:03 -0400

    added umbrella header; updated readme with latest API

---
+++ include/leo/leo.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include "leo/engine.h"
+#include "leo/image.h"
+#include "leo/font.h"
+#include "leo/audio.h"
+#include "leo/graphics.h"
+#include "leo/keyboard.h"
+#include "leo/mouse.h"
+#include "leo/gamepad.h"
+#include "leo/collisions.h"
+#include "leo/io.h"
+#include "leo/error.h"
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
