cdecec371c1b43a58adbfe302a884e4a1b2d7729
bluesentinelsec <bluesentinel@protonmail.com>
2025-09-28 00:51:09 -0400

    added variations

--- include/leo/transitions.h
+++ include/leo/transitions.h
@@ -12,9 +12,18 @@ extern "C"
 {
 #endif
 
+    typedef enum
+    {
+        LEO_TRANSITION_FADE_IN,     // Fade from opaque to transparent
+        LEO_TRANSITION_FADE_OUT,    // Fade from transparent to opaque
+        LEO_TRANSITION_CIRCLE_IN,   // Circle shrinks to reveal scene
+        LEO_TRANSITION_CIRCLE_OUT   // Circle grows to cover scene
+    } leo_TransitionType;
+
     // Simple global transition system
     LEO_API void leo_StartFadeIn(float duration, leo_Color color);
     LEO_API void leo_StartFadeOut(float duration, leo_Color color, void (*on_complete)(void));
+    LEO_API void leo_StartTransition(leo_TransitionType type, float duration, leo_Color color, void (*on_complete)(void));
     LEO_API void leo_UpdateTransitions(float dt);
     LEO_API void leo_RenderTransitions(void);
     LEO_API bool leo_IsTransitioning(void);
--- src/transitions.c
+++ src/transitions.c
@@ -3,11 +3,12 @@
 #include "leo/graphics.h"
 #include <stdbool.h>
 #include <stddef.h>
+#include <math.h>
 
 typedef struct
 {
     bool active;
-    bool fade_in; // true = fade in, false = fade out
+    leo_TransitionType type;
     float progress;
     float duration;
     leo_Color color;
@@ -18,18 +19,18 @@ static GlobalTransition g_transition = {0};
 
 void leo_StartFadeIn(float duration, leo_Color color)
 {
-    g_transition.active = true;
-    g_transition.fade_in = true;
-    g_transition.progress = 0.0f;
-    g_transition.duration = duration;
-    g_transition.color = color;
-    g_transition.on_complete = NULL;
+    leo_StartTransition(LEO_TRANSITION_FADE_IN, duration, color, NULL);
 }
 
 void leo_StartFadeOut(float duration, leo_Color color, void (*on_complete)(void))
 {
+    leo_StartTransition(LEO_TRANSITION_FADE_OUT, duration, color, on_complete);
+}
+
+void leo_StartTransition(leo_TransitionType type, float duration, leo_Color color, void (*on_complete)(void))
+{
     g_transition.active = true;
-    g_transition.fade_in = false;
+    g_transition.type = type;
     g_transition.progress = 0.0f;
     g_transition.duration = duration;
     g_transition.color = color;
@@ -59,21 +60,42 @@ void leo_RenderTransitions(void)
     if (!g_transition.active)
         return;
 
-    float alpha;
-    if (g_transition.fade_in)
-    {
-        // Fade-in: start opaque, fade to transparent
-        alpha = 1.0f - g_transition.progress;
-    }
-    else
-    {
-        // Fade-out: start transparent, fade to opaque
-        alpha = g_transition.progress;
-    }
+    int screen_width = leo_GetScreenWidth();
+    int screen_height = leo_GetScreenHeight();
 
-    leo_Color fade_color = g_transition.color;
-    fade_color.a = (unsigned char)(255 * alpha);
-    leo_DrawRectangle(0, 0, leo_GetScreenWidth(), leo_GetScreenHeight(), fade_color);
+    switch (g_transition.type)
+    {
+    case LEO_TRANSITION_FADE_IN: {
+        // Fade-in: start opaque, fade to transparent
+        float alpha = 1.0f - g_transition.progress;
+        leo_Color fade_color = g_transition.color;
+        fade_color.a = (unsigned char)(255 * alpha);
+        leo_DrawRectangle(0, 0, screen_width, screen_height, fade_color);
+        break;
+    }
+    case LEO_TRANSITION_FADE_OUT: {
+        // Fade-out: start transparent, fade to opaque
+        float alpha = g_transition.progress;
+        leo_Color fade_color = g_transition.color;
+        fade_color.a = (unsigned char)(255 * alpha);
+        leo_DrawRectangle(0, 0, screen_width, screen_height, fade_color);
+        break;
+    }
+    case LEO_TRANSITION_CIRCLE_IN: {
+        // Circle-in: large circle shrinks to reveal scene
+        float max_radius = sqrtf(screen_width * screen_width + screen_height * screen_height) / 2.0f;
+        float radius = max_radius * (1.0f - g_transition.progress);
+        leo_DrawCircle(screen_width / 2, screen_height / 2, radius, g_transition.color);
+        break;
+    }
+    case LEO_TRANSITION_CIRCLE_OUT: {
+        // Circle-out: small circle grows to cover scene
+        float max_radius = sqrtf(screen_width * screen_width + screen_height * screen_height) / 2.0f;
+        float radius = max_radius * g_transition.progress;
+        leo_DrawCircle(screen_width / 2, screen_height / 2, radius, g_transition.color);
+        break;
+    }
+    }
 }
 
 bool leo_IsTransitioning(void)
