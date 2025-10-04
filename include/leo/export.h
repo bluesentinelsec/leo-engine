6fc797665865575fd50adcd70a8c20315da0f763
bluesentinelsec <bluesentinel@protonmail.com>
2025-10-04 10:08:10 -0400

    debugging cicd (windows)

--- include/leo/export.h
+++ include/leo/export.h
@@ -10,9 +10,9 @@ extern "C"
     #include "leo/leo_export_generated.h"
     #define LEO_API LEO_EXPORT
 #else
-    // Fallback for manual builds
+    // Fallback for manual builds or when linking statically
     #if defined(_WIN32) || defined(__CYGWIN__)
-        #ifdef LEO_STATIC_DEFINE
+        #if defined(LEO_STATIC_DEFINE) || defined(TESTING)
             #define LEO_API
         #else
             #define LEO_API __declspec(dllimport)
